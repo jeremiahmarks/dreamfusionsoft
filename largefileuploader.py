96b98244c0d1c5493c3b931ba3ac3534b8f758a0
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-09-18 09:06:18 -0700

    filesfromwork

---
+++ largefileuploader.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: jeremiah.marks
+# @Date:   2015-09-17 10:18:55
+# @Last Modified 2015-09-17 @Last Modified time: 2015-09-17 10:18:55
+
+
+# This script is designed to upload one large csv file
---
+++ subscriptionsStuff.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: jeremiah.marks
+# @Date:   2015-09-17 10:25:00
+# @Last Modified 2015-09-17 @Last Modified time: 2015-09-17 10:25:00
+
+# This file will take a csv file which has the required variables needed for a historical subsccriptsion import
+# It will then import the CSV, create the needed invoices, delete the additional invoices, and set the 
+# subscription end date as well as set it to inactive. 
+
